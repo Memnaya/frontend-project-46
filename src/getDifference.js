8502db7986918316fc6ae2f9876754c40f815f92
KarinaBuga <memnaya@mail.ru>
2023-03-13 14:15:00 +0300

    make src

---
+++ src/getDifference.js
@@ -0,0 +1,32 @@
+import * as fs from 'node:fs';
+import * as path from 'node:path';
+// загрузить лодаш
+
+const getPath = fs.readFileSync(path.resolve(filepath), 'utf-8');
+const getParse = JSON.parse(data);
+
+const genDiff = (filepath1, filepath2) => {
+  const data1 = getParse(getPath(filepath1));
+  const data2 = getParse(getPath(filepath2));
+
+  const keys1 = Object.keys(data1);
+  const keys2 = Object.keys(data2);
+  const keys = _.union(keys1, keys2);
+
+  const result = {};
+  for (const key of keys) {
+    if (!Object.hasOwn(data1, key)) {
+      result[key] = 'lala';
+    } else if (!Object.hasOwn(data2, key)) {
+      result[key] = 'lala';
+    } else if (data1[key] !== data2[key]) {
+      result[key] = 'lala';
+    } else {
+      result[key] = 'lala';
+    }
+  }
+
+  return result;
+};
+
+export { genDiff };
