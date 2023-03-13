6f9f36f4f7b614c53ad8399c788117655fe770e0
KarinaBuga <memnaya@mail.ru>
2023-03-13 14:07:01 +0300

    update gendiff command

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,13 +1,15 @@
 #!/usr/bin/env node
 
-const { Command } = require('commander');
-const program = new Command();
+import { program } from 'commander';
+import gendiff from '../index.js';
 
-  program
+program
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0', '-V, --version', 'output the version number')
   .arguments('<filepath1> <filepath2>')
   .option('-f, --format <type>', 'output format')
+  .action((filepath1, filepath2) => {
+    console.log(gendiff(filepath1, filepath2));
+  });
 
-program.parse(); 
-
+program.parse();
