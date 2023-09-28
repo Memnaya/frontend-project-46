c3d51536208f6f82b9e638aa276127ad34b99c17
Karina Buga <memnaya@mail.ru>
2023-09-28 14:32:49 +0000

    refactoring getDiff

--- src/getDifference.js
+++ src/getDifference.js
@@ -6,21 +6,23 @@ const buidTree = (data1, data2) => {
   const keys = getKeys(data1, data2);
 
   const diff = keys.map((key) => {
-    if (!_.has(data1, key)) {
-      return { type: 'added', key, value: data2[key] };
-    }
-    if (!_.has(data2, key)) {
-      return { type: 'removed', key, value: data1[key] };
-    }
-    if (_.isPlainObject(data1[key]) && _.isPlainObject(data2[key])) {
-      return { type: 'nested', key, children: buidTree(data1[key], data2[key]) };
-    }
-    if (_.isEqual(data1[key], data2[key])) {
-      return { type: 'unchanged', key, value: data1[key] };
-    }
-    return {
+    let result = {
       type: 'changed', key, value1: data1[key], value2: data2[key],
     };
+
+    if (!_.has(data1, key)) {
+      result = { type: 'added', key, value: data2[key] };
+    }
+    if (!_.has(data2, key)) {
+      result = { type: 'removed', key, value: data1[key] };
+    }
+    if (_.isPlainObject(data1[key]) && _.isPlainObject(data2[key])) {
+      result = { type: 'nested', key, children: buidTree(data1[key], data2[key]) };
+    }
+    if (_.isEqual(data1[key], data2[key])) {
+      result = { type: 'unchanged', key, value: data1[key] };
+    }
+    return result;
   });
 
   return diff;
